8198a51526e1f74538e2e4eae8d26d5b92e32ebf
Josh <joshokeeffecollege@gmail.com>
2026-03-13 12:45:16 +0000

    Revert changes

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
+sudo apt-get update
+sudo apt-get install -y curl
+
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+sudo npm install -g pm2
+
 cd ~/SimpleApplication
+
+echo "$PRIVATE_KEY" > privatekey.pem
+echo "$SERVER" > server.crt
+
+chmod 600 privatekey.pem
+chmod 644 server.crt
 
 npm install
 
