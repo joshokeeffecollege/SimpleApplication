3d8d1410fc11c85e3dd5314c04e34d5be450b517
Josh <joshokeeffecollege@gmail.com>
2026-03-13 18:22:02 +0000

    Update deploy.sh and config.yml

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,10 @@ sudo npm install -g pm2
 
 cd ~/SimpleApplication
 
-printf "%s" "$PRIVATE_KEY" > privatekey.pem
-printf "%s" "$SERVER" > server.crt
+if [ ! -f privatekey.pem ] || [ ! -f server.crt ]; then
+  echo "HTTPS certificate files are missing."
+  exit 1
+fi
 
 chmod 600 privatekey.pem
 chmod 644 server.crt
@@ -21,5 +23,5 @@ npm install
 
 pm2 stop SimpleApplication || true
 pm2 delete SimpleApplication || true
-pm2 start ./bin/www --name SimpleApplication --update-env
+pm2 start ./bin/www --name SimpleApplication
 pm2 save
