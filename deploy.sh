a80f5ebadb9e137eb1ea49698ff19736220f0d7a
Josh <joshokeeffecollege@gmail.com>
2026-03-13 10:35:35 +0000

    Update deploy.sh, config.yml, and www

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-sudo apt update
-sudo apt install -y curl
+sudo apt-get update
+sudo apt-get install -y curl
 
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
-sudo apt install -y nodejs
+sudo apt-get install -y nodejs
 
 sudo npm install -g pm2
 
 cd ~/SimpleApplication
 
+echo "$PRIVATE_KEY" > privatekey.pem
+echo "$SERVER" > server.crt
+
+chmod 600 privatekey.pem
+chmod 644 server.crt
+
 npm install
 
 pm2 stop SimpleApplication || true
 pm2 delete SimpleApplication || true
-
 pm2 start ./bin/www --name SimpleApplication
 pm2 save
